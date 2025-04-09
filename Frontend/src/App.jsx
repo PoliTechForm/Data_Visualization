18f11f1b794d5344287b8eae59ee602f650a75b5
Emilio <emiliojoaquinortizmalich@gmail.com>
2025-04-09 17:07:28 -0300

    se creo el home y se estilizo

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Facebook from './components/Home'
 import { Nav } from './components/Nav'
 
 function App() {
@@ -7,8 +8,11 @@ function App() {
     <>
 
       <Nav />
-      <h1>hola</h1>
-      <h2>chau</h2>
+      <Facebook />
+      <div>
+        <h1 className='text-center text-3xl font-bold'>Hola</h1>
+        <p className='text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.</p>
+      </div>
     </>
   )
 }
--- Frontend/src/components/Nav.jsx
+++ Frontend/src/components/Nav.jsx
@@ -4,10 +4,10 @@ export const Nav = () => {
     return (
         <div className='justify-center flex bg-slate-500 text-center'>
 
-            <div>
-                <ul className='flex gap-5'>
+            <div className=''>
+                <ul className='flex gap-10 p-5 text-white hover:text-3xl transition-all duration-300 ease-in-out'>
                     <a href="">
-                        <li>Inicio</li>
+                        <li className=''>Inicio</li>
                     </a>
                     <a href="">
                         <li>Productos</li>
